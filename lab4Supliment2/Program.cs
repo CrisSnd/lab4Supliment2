ec9bea983e20136ed11ab74c53b63d12a6531d1b
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-17 22:06:58 +0300

    Add project files.

---
+++ lab4Supliment2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrie un program care afiseaza numerele de la 1 la N. Daca numarul este multiplu de 3, sa se afiseze Fizz, daca este multiplu de 3 sa se afiseze Buzz, 
+             * daca e multiplu si de 3 si de 5 (ex: 15) sa se afiseze FizzBuzz, in restul cazurilor sa se afiseze numarul..*/
+
+
+            Console.WriteLine("Introduceti  numarul n: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] intNumere = new int[n];
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+
+         }
+
+
+     }
+
+}
+
+
+    
+
+
